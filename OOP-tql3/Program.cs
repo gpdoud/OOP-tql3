9b88526999efe535681475c39013c485d3dd9f76
Greg Doud <gpdoud@gmail.com>
2024-06-17 14:59:55 -0400

    Add project files.

---
+++ OOP-tql3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OOP_tql3;
+
+internal class Program {
+
+    static void Main(string[] args) {
+
+        SUV suv = new SUV();
+        suv.Print();
+
+    }
+}
+
+public class Vehicle {
+
+    public int NbrOfWheels { get; set; } = 4;
+    public bool SlidingDoor { get; set; } = true;
+    public int NbrOfPassengers { get; set; } = 7;
+
+    public void Print(int nbr, string message) {
+
+    }
+    public void Print(string message, int nbr) {
+
+    }
+
+
+}
+
+public class SUV : Vehicle {
+
+
+    public SUV() {
+        Print("ABC")
+    }
+
+}
